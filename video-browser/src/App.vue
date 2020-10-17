30ea3988bff3d2ac8806e764831e3be9c425653c
Donghwi Park <donghwipark@gmail.com>
2020-10-17 23:36:07 +0900

    videos project

---
+++ video-browser/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <div class="row">
+      <VideoDetail :video="selectedVideo" />
+      <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+
+const API_KEY = 'AIzaSyBqabXn0ax4ecvUGsjlf9kT52H6T-CVUEI';
+
+export default {
+  name: 'App',
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail
+  },
+  data() {
+    return {
+      videos: [],
+      selectedVideo: []
+    };
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      }).then(response => {
+        this.videos = response.data.items
+      });
+    },
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    }
+  }
+};
+</script>
